48d50680abb0557c629ff2360f645cbdb5f6253c
thoratica <me@tica.fun>
2021-07-28 16:28:57 +0900

    #4 설정 변경 시 새로고침 안내 토스트 추가

--- popup.js
+++ popup.js
@@ -2,7 +2,9 @@ window.addEventListener('load', () => {
   const alwaysShow = document.getElementById('always-show');
   const useLegacy = document.getElementById('use-legacy');
 
-  [alwaysShow, useLegacy].forEach((el) =>
+  const toast = new bootstrap.Toast(document.querySelector('.toast'));
+
+  [(alwaysShow, useLegacy)].forEach((el) =>
     chrome.runtime.sendMessage({ key: el.id, method: 'get' }, (value) => {
       el.checked = value[el.id];
       el.parentNode.querySelector('.text').textContent = value[el.id] ? '활성화' : '비활성화';
@@ -10,10 +12,11 @@ window.addEventListener('load', () => {
   );
 
   [alwaysShow, useLegacy].forEach((el) =>
-    el.addEventListener('click', () => {
+    el.addEventListener('click', () =>
       chrome.runtime.sendMessage({ key: el.id, value: el.checked, method: 'set' }, () => {
         el.parentNode.querySelector('.text').textContent = el.checked ? '활성화' : '비활성화';
-      });
-    })
+        toast.show();
+      })
+    )
   );
 });
